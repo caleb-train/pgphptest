e8c3871a344b86fb637fb603414c130d517fe0f0
caleb-42 <ewisco316@yahoo.com>
2019-11-21 14:03:54 +0100

    create-model-migration-seed
    - Pass in database config credentials to env file
    - Change fields in user model and migration to fit application's needs
    - Create User Seeder and adjust UserFactory fields

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
+
 use App\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
@@ -19,8 +20,7 @@ use Illuminate\Support\Str;
 $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
+        'comments' => $faker->sentence(4),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
